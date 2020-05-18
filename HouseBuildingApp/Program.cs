f5122629cc6e89e416f5e9f4338ce7788ff40d65
jbronxxx <jbronxxx@gmail.com>
2020-05-19 00:27:37 +0500

    First Update

--- HouseBuildingApp/Program.cs
+++ HouseBuildingApp/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace HouseBuildingApp
+﻿using System;
+
+namespace HouseBuildingApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.ReadLine();
         }
     }
 }
